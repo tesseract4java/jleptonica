6f3c1ed0612657d29a2cfb1bb878c5531ba9a3c0
Paul Vorbach <paul@vorba.ch>
2014-05-08 14:08:51 +0200

    Prefer BridJ version

--- jnaerate.sh
+++ jnaerate.sh
@@ -3,11 +3,12 @@
 rm -rf src/main/java/de
 
 # call jnaerator
-java -jar jnaerator-0.12.jar \
+java -jar jnaerator-0.12-SNAPSHOT.jar \
     -library leptonica \
-    -runtime JNA \
+    -runtime BridJ \
     -direct \
     -mode Directory \
+    -skipDeprecated \
     -o src/main/java \
     -package de.vorb.leptonica \
     ../VS2013/tesseract-vs2013/liblept/include/allheaders.h
