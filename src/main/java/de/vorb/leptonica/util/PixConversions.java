e8e1941bee7c0f93c1b26bed9495380ff25b2ba4
Paul Vorbach <paul@vorba.ch>
2014-05-08 14:35:34 +0200

    Initial draft of PixConversions

---
+++ src/main/java/de/vorb/leptonica/util/PixConversions.java
@@ -0,0 +1,18 @@
+package de.vorb.leptonica.util;
+
+import java.awt.image.BufferedImage;
+
+import de.vorb.leptonica.Pix;
+
+public class PixConversions {
+    private PixConversions() {
+    }
+
+    public static BufferedImage pix2img(Pix pix) {
+        return null;
+    }
+
+    public static Pix img2pix(BufferedImage img) {
+        return null;
+    }
+}
